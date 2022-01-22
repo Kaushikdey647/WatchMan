aa3e0a49441e34fed014fb1c6eefe1ba8ae84455
Kaushik Dey <kaushikdey647@gmail.com>
2022-01-22 21:44:33 +0530

    [bug]: serial comms attempt #1

---
+++ python/dattime.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+t = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")+".avi"
+
+print(t)
--- python/pyserial.py
+++ python/pyserial.py
@@ -5,5 +5,10 @@ arduino = serial.Serial(port='COM12', baudrate=9600, timeout=.1)
 while True:
     str = arduino.readline().decode('utf-8').strip() # format the input data
     if str:
-        dist = float(str)
-        print("Object Dist: ",dist)
+        try:
+            dist = float(str)
+            print("Object Dist: ",dist)
+        except:
+            print("error",str)
+        finally:
+            print("eos")
