149bd1bbba203b51292799d398caf52be0ace431
Kaushik Dey <kaushikdey647@gmail.com>
2022-01-23 15:18:35 +0530

    Create docs.py

---
+++ docs.py
@@ -0,0 +1,39 @@
+
+#!!! NOT TO BE EXECUTED !!!
+
+"""
+HELLO THERE AND WELCOME TO THE PROJECT,
+
+I know you might be very very eager to start working on the project, but..
+
+There are some rules about this:
+
+1. Please look up the issues and try to work on them priority wise
+2. <fill in your rule here>
+3. If a file has more than 200 lines of code, it is a BIG file, try and split it
+4. Use proper docstrings for every class and every function.
+  4.1. The format for function is
+    '''
+    <Summary>
+
+    Parameters:
+    <arg1> (<type>): <description>
+    <arg2> (<type>): <description>
+    <arg3> (<type>): <description>
+    ...
+
+    Returns:
+    <type>: <description>
+    '''
+
+    this will later be visible in help(function)
+
+5. But follow the following documentation conventions
+  * Highlighted Comment
+  ! Warning Comment
+  ? In Doubt
+  TODO: A Todo ofcourse
+
+
+  BETTER DOCUMENTATION == LONGER CODE LIFE
+"""
